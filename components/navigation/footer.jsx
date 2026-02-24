be3afbcd217c812844d7eac8fea40c91d326f4cb
Manojnaik123 <manojnaikbse@gmail.com>
2026-02-24 21:22:41 +0530

    first

--- components/navigation/footer.jsx
+++ components/navigation/footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-white-bg w-full flex justify-center items-center h-14 md:h-16 border-muted-text/10 border-t text-muted-text/80'>
-            <div className="w-full max-w-5xl text-center flex flex-col gap-1">
-                <span>Powered by AI-Enhanced Intent Suggestionss | Co-powered by Bino</span>
-                <span className='text-xs text-muted-text/50'>Helping you craft clear, effective requests every time.</span>
+        <div className='bg-white-bg w-full flex justify-center items-center  border-muted-text/10 border-t text-muted-text/80'>
+            <div className="w-full max-w-5xl text-center flex flex-col gap-1 p-4">
+                <span className='text-xs md:text-md'>Powered by AI-Enhanced Intent Suggestionss</span>
+                <span className='text-[8px] md:text-[10px] text-muted-text/50'>Helping you craft clear, effective requests every time.</span>
             </div>
         </div>
     )
